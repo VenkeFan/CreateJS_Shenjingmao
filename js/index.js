3a8437b68b5f3e62f79ec70389d94c98d1734051
VenkeFan <fanqi_0817@live.com>
2015-07-27 15:22:40 +0800

    优化算法

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ var _stage = null;
 var _itemsInLine = 9; // 每行的个数
 var _sprite; // 精灵
 var _circleArray; // 存放所有圆的的二维数组
-var _stepCount = 0; //步数 
 
 window.onload = function(){
     init();
@@ -48,7 +47,6 @@ function createBackgroundCircle(container){
 
             if(Math.random() < 0.1){
                 circle.setCircleType(Circle.Type_Selected);
-                console.log(i+"-"+j);
             }
             else{
                 circle.setCircleType(Circle.Type_Normal);
@@ -62,58 +60,81 @@ function createBackgroundCircle(container){
            
             circle.x = originX+j*(radius*2+5);
             circle.y = originY+i*(radius*2+5);
-            circle.indexX = i; // 行数
-            circle.indexY = j; // 列数
-
+            circle.indexX = i; // 第几行
+            circle.indexY = j; // 第几列
             _circleArray[circle.indexX][circle.indexY] = circle;
 
             container.addChild(circle);
 
 
-            var txt = new createjs.Text(i+"-"+j, "13px Arial", "#000000");
-            txt.x = circle.x-5;
-            txt.y = circle.y-5;
-            container.addChild(txt);
+            // var txt = new createjs.Text(i+"-"+j, "13px Arial", "#000000");
+            // txt.x = circle.x-5;
+            // txt.y = circle.y-5;
+            // container.addChild(txt);
        };
     };
-
-    // console.log(_circleArray);
 }
 
 // 点击背景圆改变颜色
 function circleClick(event){
-    // 点击一次后移除点击事件
-    event.currentTarget.removeEventListener("click", circleClick);
-
-    _stepCount++;
     var clickedCircle = event.currentTarget;
-    clickedCircle.setCircleType(Circle.Type_Selected);
+    if(clickedCircle._circleType == Circle.Type_Cat || clickedCircle._circleType == Circle.Type_Selected){
+        return;
+    }
 
+    if(_sprite.indexX == 0 || _sprite.indexX == _itemsInLine-1 || _sprite.indexY == 0 || _sprite.indexY == _itemsInLine-1){
+        alert("Game Over!");
+        return;
+    }
+
+    clickedCircle.setCircleType(Circle.Type_Selected);
     getMoveRoute();
+
+    console.log(_sprite.indexX, _sprite.indexY);
 
     _stage.update();
 }
 
 // 改变精灵的位置
 function changeSpriteLocation(orientaion){
-   switch(orientaion){
+    switch(orientaion){
         case MoveOrientationEnum.TOPLEFT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX-1][(_sprite.indexX-1)%2!=0?_sprite.indexY-1:_sprite.indexY];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
         case MoveOrientationEnum.LEFT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX][_sprite.indexY-1];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
         case MoveOrientationEnum.BOTTOMLEFT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX+1][(_sprite.indexX+1)%2!=0?_sprite.indexY-1:_sprite.indexY];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
         case MoveOrientationEnum.BOTTOMRIGHT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX+1][(_sprite.indexX+1)%2==0?_sprite.indexY+1:_sprite.indexY];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
         case MoveOrientationEnum.RIGHT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX][_sprite.indexY+1];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
         case MoveOrientationEnum.TOPRIGHT:
+            _sprite.setCircleType(Circle.Type_Normal);
+            _sprite = _circleArray[_sprite.indexX-1][(_sprite.indexX-1)%2==0?_sprite.indexY+1:_sprite.indexY];
+            _sprite.setCircleType(Circle.Type_Cat);
             break;
-   }
+    }
 }
 
 // 获取精灵移动的路线
 function getMoveRoute(){
+    var distanceArray = new Array(); // 各个方向可移动的最大距离集合
+
     // 左上
     var isCan = true;
     var orientaion = MoveOrientationEnum.TOPLEFT;
@@ -122,10 +143,13 @@ function getMoveRoute(){
         if(i % 2 != 0){
             indexY--;
         }
+        if(indexY < 0){
+            break;
+        }
         if(_circleArray[i][indexY]._circleType == Circle.Type_Selected){
             isCan = false;
-        }else{
-            
+            distanceArray[MoveOrientationEnum.TOPLEFT] = _sprite.indexX-i-1;
+            break;
         }
     };
     if(isCan){
@@ -139,6 +163,8 @@ function getMoveRoute(){
     for (var i = _sprite.indexY - 1; i >= 0; i--) {
         if(_circleArray[_sprite.indexX][i]._circleType == Circle.Type_Selected){
             isCan = false;
+            distanceArray[MoveOrientationEnum.LEFT] = _sprite.indexY-i-1;
+            break;
         }
     };
     if(isCan){
@@ -154,8 +180,13 @@ function getMoveRoute(){
         if(i % 2 != 0){
             indexY--;
         }
+        if(indexY < 0){
+            break;
+        }
         if(_circleArray[i][indexY]._circleType == Circle.Type_Selected){
             isCan = false;
+            distanceArray[MoveOrientationEnum.BOTTOMLEFT] = i-_sprite.indexX-1;
+            break;
         } 
     };
     if(isCan){
@@ -171,8 +202,13 @@ function getMoveRoute(){
         if(i % 2 == 0){
             indexY++;
         }
+        if(indexY > 8){
+            break;
+        }
         if(_circleArray[i][indexY]._circleType == Circle.Type_Selected){
             isCan =false;
+            distanceArray[MoveOrientationEnum.BOTTOMRIGHT] = i-_sprite.indexX-1;
+            break;
         }
     }
     if(isCan){
@@ -186,6 +222,8 @@ function getMoveRoute(){
     for (var i = _sprite.indexY + 1; i < _itemsInLine; i++) {
         if(_circleArray[_sprite.indexX][i]._circleType == Circle.Type_Selected){
             isCan = false;
+            distanceArray[MoveOrientationEnum.RIGHT] = i-_sprite.indexY-1;
+            break;
         }
     };
     if(isCan){
@@ -201,13 +239,34 @@ function getMoveRoute(){
         if(i % 2 == 0){
             indexY++;
         }
+        if(indexY > 8){
+            break;
+        }
         if(_circleArray[i][indexY]._circleType == Circle.Type_Selected){
             isCan = false;
+            distanceArray[MoveOrientationEnum.TOPRIGHT] = _sprite.indexX-i-1;
+            break;
         }
     }
     if(isCan){
         changeSpriteLocation(orientaion);
         return;
+    }
+
+    var maxDistance = -1, minDistance = -1;
+    for (var i = 0; i < distanceArray.length; i++) {
+        if(distanceArray[i] > maxDistance){
+            maxDistance = distanceArray[i];
+            orientaion = i;
+        }
+    };
+
+    // console.log("------->", maxDistance, orientaion);
+
+    if(maxDistance > 0){
+        changeSpriteLocation(orientaion);
+    }else{
+        alert("You Win!");
     }
 }
 
